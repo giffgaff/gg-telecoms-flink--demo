f4f00a6982523388a77717f27804d05e3b62f6b0
gabor.csuvik <gabor.csuvik@infinitelambda.com>
2023-06-22 14:28:34 +0200

    Share project

---
+++ src/main/java/com/infinitelambda/DataStreamJob.java
@@ -0,0 +1,75 @@
+package com.infinitelambda;
+
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.common.serialization.SerializationSchema;
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.connector.base.DeliveryGuarantee;
+import org.apache.flink.connector.kafka.sink.KafkaRecordSerializationSchema;
+import org.apache.flink.connector.kafka.sink.KafkaSink;
+import org.apache.flink.connector.kafka.source.KafkaSource;
+import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.util.Collector;
+
+import java.nio.charset.StandardCharsets;
+
+
+public class DataStreamJob {
+
+    public static void main(String[] args) throws Exception {
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        KafkaSource<String> source = KafkaSource.<String>builder()
+                .setBootstrapServers("localhost:9092")
+                .setTopics("word-count-input")
+                .setGroupId("flink-wordcount")
+                .setStartingOffsets(OffsetsInitializer.earliest())
+                .setValueOnlyDeserializer(new SimpleStringSchema())
+                .build();
+
+        DataStreamSource<String> text = env.fromSource(source, WatermarkStrategy.noWatermarks(), "kafka-source");
+
+        DataStream<Tuple2<String, Integer>> counts = text
+                .flatMap(new Tokenizer())
+                .keyBy(item -> item.f0)
+                .sum(1);
+
+        KafkaSink<Tuple2<String, Integer>> sink = KafkaSink.<Tuple2<String, Integer>>builder()
+                .setBootstrapServers("localhost:9092")
+                .setRecordSerializer(KafkaRecordSerializationSchema.builder()
+                        .setTopic("word-count-output")
+                        .setValueSerializationSchema(new SerSchema())
+                        .build()
+                )
+                .setDeliveryGuarantee(DeliveryGuarantee.AT_LEAST_ONCE)
+                .build();
+
+        counts.sinkTo(sink);
+
+        env.execute("Kafka WordCount");
+    }
+
+    public static final class Tokenizer implements FlatMapFunction<String, Tuple2<String, Integer>> {
+        @Override
+        public void flatMap(String value, Collector<Tuple2<String, Integer>> out) {
+            String[] words = value.toLowerCase().split("\\W+");
+
+            for (String word : words) {
+                if (word.length() > 0) {
+                    out.collect(new Tuple2<>(word, 1));
+                }
+            }
+        }
+    }
+
+    private static final class SerSchema implements SerializationSchema<Tuple2<String, Integer>> {
+        @Override
+        public byte[] serialize(Tuple2<String, Integer> stringIntegerTuple2) {
+            return (stringIntegerTuple2.f0 + ":" + stringIntegerTuple2.f1.toString()).getBytes(StandardCharsets.UTF_8);
+        }
+    }
+}
